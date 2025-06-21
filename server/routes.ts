8866c8a0b06bd0dd078af3d8583ce8bcb1ee64e5
Quickplus <79933823+Quickplus2022@users.noreply.github.com>
2025-06-21 16:30:39 -0300

    Update routes.ts

--- server/routes.ts
+++ server/routes.ts
@@ -3,7 +3,7 @@ import { createServer, type Server } from "http";
 import multer from "multer";
 import path from "path";
 import fs from "fs";
-import { readExcelFile, parseExcelDataToLenses, getAvailableOptions } from "./excel-reader";
+import { readExcelFile } from "./excel-reader";
 
 const upload = multer({ dest: 'uploads/' });
 
